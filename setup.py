0bf2fd0ea16ba876d842ce43f49a8c3ff0124e5a
LDprg <lukas_4dr@gmx.at>
2023-10-10 12:39:36 +0200

    fix test

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+setup(
+    install_require = [
+        "logging",
+        "async-timeout",
+        "asyncio",
+        "websockets",
+        "requests",
+        'importlib-metadata; python_version<"3.8"',
+    ]
+)
